eebac4c671a2e10b0418699e9df2ecfa2f432d7f
ph4nie <f.pesle@openmailbox.org>
2015-11-29 18:58:35 +0100

    fin provisoire de désérialisation et initialisation

--- projetInfo2a/ProjetInfo2a/ClassMission.cs
+++ projetInfo2a/ProjetInfo2a/ClassMission.cs
@@ -10,16 +10,25 @@ namespace ProjetInfo2a
 {
     class ClassMission
     {
-        private List<ClassActivite> _activites;
+        private List<String> _activites;
         private int _nbAstraunautes;
         private List<String> _astronautes;
         private ClassLieu _lieu;
         private int _jourJ;
         private DateTime _t0;
+        private int _duree; //en nb de jours
         private ClassJour _journeeDefaut;
+        private Dictionary<int, ClassJour> _planning;
 
-        // methode de désérialisation globale qui appelle les methodes de chaque champ de Mission
-        public void recupXML()
+        //constructeur par défaut, qui initialise la mission en chargeant les données
+        public ClassMission()
+        {
+            chargerInfo();
+            initialisePlanning();
+        }
+
+        // désérialisation globale qui appelle les methodes de chaque champ de Mission
+        public void chargerInfo()
         {
             XmlDocument xmlDoc = new XmlDocument();
 
@@ -33,9 +42,28 @@ namespace ProjetInfo2a
                 MessageBox.Show(message);
                 return;
             }
+            load_mission(xmlDoc);
             load_activites(xmlDoc);
             load_lieu(xmlDoc);
 
+        }
+
+        // désérialisation de <mission>
+        private void load_mission(XmlDocument doc)
+        {
+            _lieu = new ClassLieu();
+            // on crée un noeud pour la balise mission
+            XmlNode node = doc.SelectSingleNode("/informations/mission");
+
+            //on recupere son t0 dans l'attribut de la balise du .xml
+            XmlAttribute xml_attr = node.Attributes["t0"];
+
+            // on affecte le champ t0 de la Mission avec l'attribut qu'on vient de récup
+            _t0 = DateTime.Parse(xml_attr.Value);
+
+            //idem pour le champ duree
+            xml_attr = node.Attributes["duree"];
+            _duree = int.Parse(xml_attr.Value);
         }
 
         // désérialisation de <lieu>
@@ -69,23 +97,21 @@ namespace ProjetInfo2a
         // désérialisation de <activites>
         private void load_activites(XmlDocument doc)
         {
-            _activites = new List<ClassActivite>();
+            _activites = new List<String>();
             XmlNodeList nl = doc.SelectNodes("/informations/activites/activite");
             foreach (XmlNode node in nl) // pour chaque activite de activites
             {
                 //on recupere sa categorie dans l'attribut de la balise du .xml
                 XmlAttribute xml_attr = node.Attributes["categorie"];
-                //on instancie un objet ClassActivite
-                ClassActivite activite = new ClassActivite();
-                //dont on affecte la propriété _categorie avec l'attribut qu'on vient de récup
-                activite.setCategorie(xml_attr.Value);
+                //on instancie un string contenant avec l'attribut qu'on vient de récup
+                String activite = xml_attr.Value;
 
                 xml_attr = node.Attributes["sous-categorie"];
-                activite.setCategorie(activite.getCategorie() + " / " + xml_attr.Value);
+                activite += " / " + xml_attr.Value;
 
                 //idem pour l'attribut nom 
                 xml_attr = node.Attributes["nom"];
-                activite.setNom(xml_attr.Value);
+                activite += " / " + xml_attr.Value;
 
                 // on ajoute l'activité à la liste d'activités de la mission
                 _activites.Add(activite);
@@ -101,20 +127,44 @@ namespace ProjetInfo2a
             {
                 String astronaute;
                 XmlAttribute xml_attr = node.Attributes["nom"];
-                astronaute=xml_attr.Value;
+                astronaute = xml_attr.Value;
 
                 _astronautes.Add(astronaute);
             }
-            _nbAstraunautes = _astronautes.Count(); 
+            _nbAstraunautes = _astronautes.Count();
         }
 
+        // désérialisation de journeeDefaut
         private void load_journeeDefaut(XmlDocument doc)
         {
             _journeeDefaut = new ClassJour();
 
+            XmlNodeList nl = doc.SelectNodes("/informations/journeeDefaut/activite");
+            foreach (XmlNode node in nl)
+            {
+                ClassActivite activite = new ClassActivite();
+                XmlAttribute xml_attr = node.Attributes["categorie"];
+                activite.setCategorie(xml_attr);
+                xml_attr = node.Attributes["nom"];
+                activite.setCategorie(activite.getCategorie() + " / " + xml_attr);
+
+                xml_attr = node.Attributes["hdebut"];
+                activite.setHeureDeb(xml_attr);
+                xml_attr = node.Attributes["hfin"];
+                activite.setHeureFin(xml_attr);
+
+                _journeeDefaut.ajouterActivite(activite);
+            }
         }
 
-
+        // remplissage du planning par défaut
+        public void initialisePlanning()
+        {
+            for (int i = 1; i <= _duree; i++)
+            {
+                _planning.Add(i, _journeeDefaut);
+            }
+        }
 
     }
 }
